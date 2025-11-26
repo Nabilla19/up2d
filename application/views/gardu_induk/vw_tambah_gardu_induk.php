1ee27b3a4c06dd5ef8e818cb25e5bc8f91f88e00
aayyu09 <rahma22ti@mahasiswa.pcr.ac.id>
2025-11-26 12:03:59 +0700

    gi-view-2

--- application/views/gardu_induk/vw_tambah_gardu_induk.php
+++ application/views/gardu_induk/vw_tambah_gardu_induk.php
@@ -1,10 +1,10 @@
 <main class="main-content position-relative border-radius-lg ">
     <!-- NAVBAR -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" 
-         id="navbarBlur" data-scroll="false">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl"
+        id="navbarBlur" data-scroll="false">
         <div class="container-fluid py-1 px-3">
             <h6 class="font-weight-bolder text-white mb-0">
-                <i class="fas fa-bolt me-2 text-warning"></i> Tambah Gardu Induk
+                <i class="fas fa-building me-2 text-success"></i> Tambah Gardu Induk
             </h6>
         </div>
     </nav>
@@ -21,117 +21,139 @@
 
                     <div class="row g-3">
 
-                        <!-- Unit Layanan -->
+                        <!-- Kolom sesuai tabel -->
+
                         <div class="col-md-6">
-                            <label class="form-label">Unit Layanan</label>
-                            <input type="text" name="UNIT_LAYANAN" class="form-control" required>
+                            <label class="form-label">UP3 2D</label>
+                            <input type="text" name="UP3_2D" class="form-control">
                         </div>
 
-                        <!-- Nama Gardu -->
                         <div class="col-md-6">
-                            <label class="form-label">Nama Gardu Induk</label>
-                            <input type="text" name="GARDU_INDUK" class="form-control" required>
+                            <label class="form-label">Unit Name UP3</label>
+                            <input type="text" name="UNITNAME_UP3" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">CX Unit</label>
+                            <input type="text" name="CXUNIT" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Unit Name</label>
+                            <input type="text" name="UNITNAME" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Location</label>
+                            <input type="text" name="LOCATION" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label class="form-label">SSOT Number</label>
+                            <input type="text" name="SSOTNUMBER" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label class="form-label">Description</label>
+                            <input type="text" name="DESCRIPTION" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Status</label>
+                            <input type="text" name="STATUS" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">TUJD Number</label>
+                            <input type="text" name="TUJDNUMBER" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Asset Class HI</label>
+                            <input type="text" name="ASSETCLASSHI" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label class="form-label">S Address Code</label>
+                            <input type="text" name="SADDRESSCODE" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label class="form-label">Classification Desc</label>
+                            <input type="text" name="CXCLASSIFICATIONDESC" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Penyulang</label>
+                            <input type="text" name="PENYULANG" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Parent</label>
+                            <input type="text" name="PARENT" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Parent Description</label>
+                            <input type="text" name="PARENT_DESCRIPTION" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Install Date</label>
+                            <input type="text" name="INSTALLDATE" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Actual Opr Date</label>
+                            <input type="text" name="ACTUALOPRDATE" class="form-control">
+                        </div>
+
+                        <div class="col-md-4">
+                            <label class="form-label">Change Date</label>
+                            <input type="text" name="CHANGEDATE" class="form-control">
+                        </div>
+
+                        <div class="col-md-6">
+                            <label class="form-label">Change By</label>
+                            <input type="text" name="CHANGEBY" class="form-control">
                         </div>
 
                         <!-- Koordinat -->
-                        <div class="col-md-3">
-                            <label class="form-label">Longitude (X)</label>
-                            <input type="text" name="LONGITUDEX" class="form-control">
-                        </div>
-
                         <div class="col-md-3">
                             <label class="form-label">Latitude (Y)</label>
                             <input type="text" name="LATITUDEY" class="form-control">
                         </div>
 
-                        <!-- Status Operasi -->
+                        <div class="col-md-3">
+                            <label class="form-label">Longitude (X)</label>
+                            <input type="text" name="LONGITUDEX" class="form-control">
+                        </div>
+
                         <div class="col-md-6">
-                            <label class="form-label">Status Operasi</label>
-                            <select name="STATUS_OPERASI" class="form-control">
-                                <option value="">-- Pilih Status --</option>
-                                <option value="OPERATING">OPERATING</option>
-                                <option value="NOT READY">NOT READY</option>
-                                <option value="INACTIVE">INACTIVE</option>
-                                <option value="REQOPERATING">REQOPERATING</option>
-                            </select>
+                            <label class="form-label">Formatted Address</label>
+                            <input type="text" name="FORMATTEDADDRESS" class="form-control">
                         </div>
 
-                        <!-- Data teknis -->
-                        <div class="col-md-2">
-                            <label class="form-label">Jumlah TD</label>
-                            <input type="number" name="JML_TD" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">INC</label>
-                            <input type="number" name="INC" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">OGF</label>
-                            <input type="number" name="OGF" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">Spare</label>
-                            <input type="number" name="SPARE" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">Couple</label>
-                            <input type="number" name="COUPLE" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">Bus Riser</label>
-                            <input type="number" name="BUS_RISER" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">BBVT</label>
-                            <input type="number" name="BBVT" class="form-control">
-                        </div>
-
-                        <div class="col-md-2">
-                            <label class="form-label">PS</label>
-                            <input type="number" name="PS" class="form-control">
-                        </div>
-
-                        <!-- Status SCADA -->
-                        <div class="col-md-4">
-                            <label class="form-label">Status SCADA</label>
-                            <select name="STATUS_SCADA" class="form-control">
-                                <option value="">-- Pilih Status SCADA --</option>
-                                <option value="INTEGRASI">INTEGRASI</option>
-                                <option value="NON INTEGRASI">NON INTEGRASI</option>
-                            </select>
-                        </div>
-
-                        <!-- IP & RTU -->
-                        <div class="col-md-4">
-                            <label class="form-label">IP Gateway</label>
-                            <input type="text" name="IP_GATEWAY" class="form-control">
+                        <div class="col-md-6">
+                            <label class="form-label">Street Address</label>
+                            <input type="text" name="STREETADDRESS" class="form-control">
                         </div>
 
                         <div class="col-md-4">
-                            <label class="form-label">IP RTU</label>
-                            <input type="text" name="IP_RTU" class="form-control">
+                            <label class="form-label">City</label>
+                            <input type="text" name="CITY" class="form-control">
                         </div>
 
                         <div class="col-md-4">
-                            <label class="form-label">Merk RTU</label>
-                            <input type="text" name="MERK_RTU" class="form-control">
+                            <label class="form-label">Is Asset</label>
+                            <input type="text" name="ISASSET" class="form-control">
                         </div>
 
                         <div class="col-md-4">
-                            <label class="form-label">SN RTU</label>
-                            <input type="text" name="SN_RTU" class="form-control">
+                            <label class="form-label">Status Kepemilikan</label>
+                            <input type="text" name="STATUS_KEPEMILIKAN" class="form-control">
                         </div>
 
-                        <div class="col-md-4">
-                            <label class="form-label">Tahun Integrasi</label>
-                            <input type="text" name="THN_INTEGRASI" class="form-control">
-                        </div>
                     </div>
 
                     <!-- BUTTON -->
@@ -154,6 +176,7 @@
     .form-control {
         border-radius: 8px;
     }
+
     .btn {
         border-radius: 6px;
         padding: 8px 18px;
